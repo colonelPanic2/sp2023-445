6bb027a349bd1099ae5023e8544359251fc244f8
ldavila17 <60617757+ldavila2@users.noreply.github.com>
2023-04-29 14:35:26 -0500

    Moving the callbacks outside of the gpio class definition

--- code/main.py
+++ code/main.py
@@ -26,7 +26,7 @@ def microcontroller_CTRL_ACK_handler(signum,frame): # SIGUSR1
     time_data('time','loop_end',6)
     signal.signal(signal.SIGUSR2,microcontroller_PROX_handler)
     signal.signal(signal.SIGUSR1,microcontroller_CTRL_ACK_handler)
-def microcontroller_PROX_handler(self,signum,frame): # SIGUSR2
+def microcontroller_PROX_handler(signum,frame): # SIGUSR2
     # signal.signal(signal.SIGUSR2,signal.SIG_IGN)
     print("START",end=' ')
     time_data('time','loop_init',6)
