4f9d71cca97e356fbfb1cb26fdc4c8a396086b11
ldavila17 <60617757+ldavila2@users.noreply.github.com>
2023-04-29 13:38:07 -0500

    Trying to extract more information

--- code/helpers/helpers.py
+++ code/helpers/helpers.py
@@ -157,7 +157,7 @@ def time_data(args,state,step,t0=0,noprint=0,logfile=None,num_samples=None):
                         runtimes=runtimes[1:]
                     time_data_dict[state] = (round(np.mean(np.array(runtimes)),2),len(time_data_dict[state])-int(len(time_data_dict[state])>1))
                     writefile(log_file,f"{state}\nMIN: {min(runtimes)}\nMAX: {max(runtimes)}\nAVG: {time_data_dict[state]}\nSTD: {np.std(np.array(runtimes))}\n")
-                    print(f"N:   {len(runtimes)}\n{state}\nMIN: {min(runtimes)}\nMAX: {max(runtimes)}\nAVG: {time_data_dict[state]}\nSTD: {np.std(np.array(runtimes))}")
+                    print(f"{state}\nN:   {len(runtimes)}\nMIN: {min(runtimes)}\nMAX: {max(runtimes)}\nAVG: {time_data_dict[state][0]}\nSTD: {np.std(np.array(runtimes))}")
                 else:
                     time_data_dict[state] = [0,0]
             return time_data_dict
@@ -166,6 +166,7 @@ def time_data(args,state,step,t0=0,noprint=0,logfile=None,num_samples=None):
     elif step == 4 and str(args[0])=='time':# and args[1]!=0:
         if T2!=0:
             microcontroller_time_data[state].append(round(1000*(T3-T2),2))
+        print(microcontroller_time_data[state][-1])
         # _, INT_start_time, INT_end_time = args
         # microcontroller_time_data[state].append(round((INT_end_time-INT_start_time)*1000,2))
     elif step==6 and str(args)=='time':
