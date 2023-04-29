0d6f2c38bf44ca2d435f14a8c1338a60d26e644a
ldavila17 <60617757+ldavila2@users.noreply.github.com>
2023-04-29 16:01:03 -0500

    Found an alternate way to silence signals while in the handler

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ def control_switch_handler(signum,frame):
     global fsm
     fsm.control_switch()
 def microcontroller_CTRL_ACK_handler(signum,frame): # SIGUSR1
-    # signal.signal(signal.SIGUSR1,signal.SIG_IGN)
+    signal.pthread_sigmask(signal.SIG_BLOCK,{signal.SIGUSR1, signal.SIGUSR2})
     print("END\n")
     time_data('time','',7)
     global fsm
@@ -24,12 +24,13 @@ def microcontroller_CTRL_ACK_handler(signum,frame): # SIGUSR1
         time_data([control_.gettimes,control_.INT_start_time,t1],state,4)
         control_.INT_start_time=0
     time_data('time','loop_end',6)
-    signal.signal(signal.SIGUSR2,microcontroller_PROX_handler)
-    signal.signal(signal.SIGUSR1,microcontroller_CTRL_ACK_handler)
+    signal.pthread_sigmask(signal.SIG_UNBLOCK,{signal.SIGUSR1, signal.SIGUSR2})
 def microcontroller_PROX_handler(signum,frame): # SIGUSR2
+    signal.pthread_sigmask(signal.SIG_BLOCK,{signal.SIGUSR1,signal.SIGUSR2})
     # signal.signal(signal.SIGUSR2,signal.SIG_IGN)
     print("START",end=' ')
     time_data('time','loop_init',6)
+    signal.pthread_sigmask(signal.SIG_UNBLOCK,{signal.SIGUSR1,signal.SIGUSR2})
     # self.proximity = int(not self.proximity)
     # Tell the microcontroller not to send any more proximity data until the design re-enters the ACQUIRE state
     # ctrl.communication_stop() 
